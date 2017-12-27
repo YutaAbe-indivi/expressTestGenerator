f0acce9fb641f51288802a40cc73bfa435cc2b82
abeyuta <yuta.abe@datasection.co.jp>
2017-12-27 18:38:40 +0900

    person.jsに集約

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express')
+var person = require('express')
 var router = express.Router()
 
-router.get('/person/:id([0-9]+)', require('./personShow'))
-router.get('/person/all', require('./personAllShow'))
-router.post('/person/', require('./personAdd'))
-router.put('/person/:id([0-9]+)', require('./personUpdate'))
-router.delete('/person/:id([0-9]+)', require('./personDelete'))
+router.get('/person/:id([0-9]+)', person)
+router.get('/person/all', person)
+router.post('/person/', person)
+router.put('/person/:id([0-9]+)', person)
+router.delete('/person/:id([0-9]+)', person)
 
 module.exports = router
---
+++ routes/person.js
@@ -0,0 +1,49 @@
+const mysql = require('../lib/mysqlConnection')
+const log4js = require('log4js')
+
+var express = require('express')
+const router = express.Router()
+
+log4js.configure(`config/log-config.json`)
+const logger = log4js.getLogger('sql')
+
+router.get('/person/:id([0-9]+)', (req, res) => {
+    const query = 'select * from person where id = ' + req.params.id
+    logger.info('parsonShow query : ' + query)
+    mysql.query(query, (err, rows) => {
+        res.send(rows)
+    })
+})
+
+router.get('/person/all', (req, res) => {
+    const query = 'select * from person'
+    logger.info('parsonAll query : ' + query)
+    mysql.query(query, (err, rows) => {
+        res.send(rows)
+    })
+})
+
+router.post('/person/', (req, res) => {
+    const query = 'insert into person(name) values("' + req.body.name + '")'
+    logger.info('parsonAdd query : ' + query)
+    mysql.query(query, (err, rows) => {
+        res.send(req.body.name + ' pushed.')
+    })
+})
+
+router.put('/person/:id([0-9]+)', (req, res) => {
+    const query = 'update person SET name ="' + req.body.name + '" where id =' + req.params.id
+    logger.info('parsonUpdate query : ' + query)
+    mysql.query(query, (err, rows) => {
+        res.send('id:' + req.params.id + ' updated.' + req.body.name)
+    })
+})
+
+router.delete('/person/:id([0-9]+)', (req, res) => {
+    const query = 'delete from person where id =' + req.params.id
+    logger.info('parsonDelete query : ' + query)
+    mysql.query(query, (err, rows) => {
+        res.send('id:' + req.params.id + ' deleted.')
+    })
+})
+module.exports = router
